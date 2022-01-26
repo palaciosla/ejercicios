9e5ed9fb3b66ee7eacf37d42107a9f77dbbb49dc
palaciosla <89703241+palaciosla@users.noreply.github.com>
2022-01-26 17:00:10 -0300

    add validacion-formularios

---
+++ 05-validacion-formularios/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactForm from "./components/ContactForm";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
